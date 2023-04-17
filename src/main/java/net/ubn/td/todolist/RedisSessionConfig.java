bd4a168147f4ac692f3d85058170ca20875b7f8f
CSLin <cs.lin@ubn.net>
2023-04-17 17:04:35 +0800

    加入DefaultCookieSerializer

--- src/main/java/net/ubn/td/todolist/RedisSessionConfig.java
+++ src/main/java/net/ubn/td/todolist/RedisSessionConfig.java
@@ -1,7 +1,10 @@
 package net.ubn.td.todolist;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
 
 /**
  * @author CSLin
@@ -10,4 +13,17 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 @Configuration
 @EnableRedisHttpSession(maxInactiveIntervalInSeconds = 60*60)
 public class RedisSessionConfig {
+    @Bean
+    public CookieSerializer cookieSerializer() {
+        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+        serializer.setCookieName("JSESSIONID");
+        serializer.setCookiePath("/");
+        //域名的正则表达式。^.?\\.(\\w\\.[a-z]+)$ 是个通用写法。
+        // 如果正则表达式不匹配，则不会设置任何域。如果正则表达式匹配，则第一个分组将用作域。
+        // 例如：https://child.mydomain.com的请求会将域设置为mydomain.com。
+        // http:// localhost:8080 /或https://192.168.1.100:8080/的请求将不对域名进行处理。
+        // 因此，在切换域名时，不需要再做任何修改。
+        serializer.setDomainNamePattern("^.+?\\.(\\w+\\.[a-z]+)$");
+        return serializer;
+    }
 }
